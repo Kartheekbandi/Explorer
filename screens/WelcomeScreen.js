b1531e1470015cfbb80106525a3309062eed4921
Kartheekbandi <123476304+Kartheekbandi@users.noreply.github.com>
2023-10-11 13:03:16 -0400

    sec

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,8 +1,12 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
 import axios from 'axios';
-import { useContext, useEffect, useState } from 'react';
-
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'; // Make sure to import Ionicons or an appropriate icon library.
 import { AuthContext } from '../store/auth-context';
+
+const Tab = createBottomTabNavigator();
 
 function WelcomeScreen() {
   const [fetchedMessage, setFetchedMesssage] = useState('');
@@ -21,27 +25,57 @@ function WelcomeScreen() {
       });
   }, [token]);
 
+  function LocalActivity() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Any events will be posted here!</Text>
+      </View>
+    );
+  }
+
+  function Tourist() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Tourist View!</Text>
+      </View>
+    );
+  }
+
+  function Settings() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.rootContainer}>
-      <Text style={styles.title}>Welcome!</Text>
-      <Text>You authenticated successfully!</Text>
-      <Text>{fetchedMessage}</Text>
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Settings') {
+            iconName = focused ? 'ios-list' : 'ios-list-outline';
+          } else if (route.name === 'Tourist') {
+            iconName = focused ? 'film' : 'film-outline';
+          } else if (route.name === 'Local Activity') {
+            iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Tourist" component={Tourist} />
+      <Tab.Screen name="Local Activity" component={LocalActivity} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
   );
 }
 
 export default WelcomeScreen;
 
-const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 32,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
-});
